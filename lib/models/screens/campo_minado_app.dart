f34879bc7f6f86172868b9007bf758236a837e4d
Ismael Alison <ismael_alison4@hotmail.com>
2022-11-08 22:14:30 -0300

    criação e implementacao tela campo_minado_app

---
+++ lib/models/screens/campo_minado_app.dart
@@ -0,0 +1,25 @@
+import 'package:campo_minado/components/resultado_widget.dart';
+import 'package:flutter/material.dart';
+
+class CampoMinadoApp extends StatelessWidget {
+  const CampoMinadoApp({super.key});
+
+  _reiniciar() {
+    print('reiniciar...');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: ResultadoWidget(
+          venceu: false,
+          onReiniciar: _reiniciar,
+        ),
+        body: Container(
+          child: const Text('Tabuleiro'),
+        ),
+      ),
+    );
+  }
+}
